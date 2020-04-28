5f29519ec5da4b03a124b3ff12771dc1923e8ffb
Ben Guo <benzguo@gmail.com>
2020-04-28 11:38:03 -0700

    add demo page content from worrydream

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
   import { store } from "./gun-store";
   import { format } from "timeago.js";
 
+  let windowHeight;
   let textarea;
   let value;
 
@@ -24,11 +25,35 @@
 <style>
   .meta {
     font-size: 10px;
-    /* margin: 0.5em; */
+    background-color: transparent;
   }
 
   .user {
     text-align: right;
+  }
+
+  .chat-sidebar {
+    position: absolute;
+    background-color: black;
+    width: 10px;
+    min-height: 100%;
+    /* overflow: auto; */
+    /* left: 0px; */
+  }
+
+  .chat-container {
+    position: absolute;
+    background-color: transparent;
+    width: auto;
+    padding-left: 10px;
+  }
+
+  .chat-message-list {
+    background-color: transparent;
+  }
+
+  .chat-message {
+    background-color: white;
   }
 </style>
 
@@ -39,46 +64,50 @@ To prevent this, add accessors={false} to svelte:options.
 https://dev.to/silvio/how-to-create-a-web-components-in-svelte-2g4j
 -->
 <svelte:options tag={'scroll-chat'} />
+<svelte:window bind:outerHeight={windowHeight} />
 <div>
-  <div>
-    {#each $store as val (val.msgId)}
-      <div>
-        <span class="meta">
-          <span class="time">{format(val.time)}</span>
-          <span class="user">{val.user.substr(0, 5)}</span>
-        </span>
-        <span class="meta">
-          {val.msg}
-          <button
-            on:click|preventDefault={() => {
-              const yes = confirm('Are you sure?');
-              if (yes) store.delete(val.msgId);
-            }}>
-            delete
-          </button>
-        </span>
-      </div>
-    {/each}
-  </div>
-
-  <form method="get" autocomplete="off" on:submit|preventDefault>
-    <div class="input-with-button">
-      <textarea
-        bind:this={textarea}
-        class="input"
-        type="text"
-        name="null"
-        maxLength="160"
-        bind:value
-        on:keydown={handleKeydown}
-        placeholder="message" />
-      {#if value}
-        <input
-          class="submit"
-          type="submit"
-          value="Send"
-          on:click={handleSubmit} />
-      {/if}
+  <div class="chat-sidebar" style={`height: ${windowHeight}`} />
+  <div class="chat-container">
+    <div class="chat-message-list">
+      {#each $store as val (val.msgId)}
+        <div class="chat-message">
+          <span class="meta">
+            <span class="time">{format(val.time)}</span>
+            <span class="user">{val.user.substr(0, 5)}</span>
+          </span>
+          <span class="meta">
+            {val.msg}
+            <button
+              on:click|preventDefault={() => {
+                const yes = confirm('Are you sure?');
+                if (yes) store.delete(val.msgId);
+              }}>
+              delete
+            </button>
+          </span>
+        </div>
+      {/each}
     </div>
-  </form>
+
+    <form method="get" autocomplete="off" on:submit|preventDefault>
+      <div class="input-with-button">
+        <textarea
+          bind:this={textarea}
+          class="input"
+          type="text"
+          name="null"
+          maxLength="160"
+          bind:value
+          on:keydown={handleKeydown}
+          placeholder="message" />
+        {#if value}
+          <input
+            class="submit"
+            type="submit"
+            value="Send"
+            on:click={handleSubmit} />
+        {/if}
+      </div>
+    </form>
+  </div>
 </div>
